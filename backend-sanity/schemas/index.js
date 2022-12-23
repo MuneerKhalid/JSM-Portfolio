3fc487930795ae09ee66038f13563569b260c70b
MuneerKhalid <muneer.ansari.2646@gmail.com>
2022-12-23 20:02:26 +0500

    more schemas

--- backend-sanity/schemas/index.js
+++ backend-sanity/schemas/index.js
@@ -1,7 +1,19 @@
+import about from "./about";
+import brands from "./brands";
 import contact from "./contact";
+import experience from "./experience";
+import skills from "./skills";
 import testimonials from "./testimonials";
+import work from "./work";
+import workexperience from "./workexperience";
 
 export const schemaTypes = [
   testimonials,
-  contact
+  contact,
+  about,
+  experience,
+  skills,
+  work,
+  workexperience,
+  brands
 ]
