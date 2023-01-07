ccef666fe54ea239a1740bca92661bdae514be57
MuneerKhalid <muneer.ansari.2646@gmail.com>
2023-01-07 08:42:39 +0500

    corrections in about and clientjs

--- frontend_react/src/client.js
+++ frontend_react/src/client.js
@@ -2,7 +2,7 @@ import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = sanityClient({
-  projectId: Process.env.REACT_APP__SANITY_PROJECT_ID,
+  projectId: process.env.REACT_APP__SANITY_PROJECT_ID,
   dataset: 'production',
   apiVersion: '2022-02-01',
   useCdn: 'true',
