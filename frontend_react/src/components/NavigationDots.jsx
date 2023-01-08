d6b1cec191afbfc2369921d81a0b500e23d5b25d
MuneerKhalid <muneer.ansari.2646@gmail.com>
2023-01-08 18:06:28 +0500

    changes

--- frontend_react/src/components/NavigationDots.jsx
+++ frontend_react/src/components/NavigationDots.jsx
@@ -3,14 +3,14 @@ import React from "react";
 const NavigationDots = ({ active }) => {
   return (
     <div className="app__navigation">
-      {["Home", "About", "Work", "Skills", "testimonial", "Contact"].map(
-        (item) => (
+      {["Home", "About", "Work", "Skills", "Testimonial", "Contact"].map(
+        (item, index) => (
           <a 
             href={`#${item}`} 
             key={item + index}
             className='app__navigation-dot'
             style={active === item ? { backgroundColor: '#313BAC' } : { }}
-            />
+          />
         )
       )}
     </div>
--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -74,4 +74,4 @@ const Header = () => {
   )
 }
 
-export default AppWrap(Header, 'home')
+export default AppWrap(Header, 'Home');
