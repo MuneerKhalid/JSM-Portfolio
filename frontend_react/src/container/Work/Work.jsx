6b07de4d7667a596821128869bd40dd92a5ce20f
MuneerKhalid <muneer.ansari.2646@gmail.com>
2023-01-10 10:55:24 +0500

    skills and work

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -8,8 +8,8 @@ import './Work.scss';
 
 const Work = () => {
 
-  const [activeFilter, setactiveFilter] = useState('All');
-  const [animateCard, setanimateCard] = useState({ y: 0, opacity: 1 });
+  const [activeFilter, setActiveFilter] = useState('All');
+  const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
   const [works, setWorks] = useState([]);
   const [filterWork, setFilterWork] = useState([]);
 
@@ -25,7 +25,17 @@ const Work = () => {
   
 
   const handleWorkFilter = (item) => {
+    setActiveFilter(item);
+    setAnimateCard([{y:100, opacity: 0}]);
 
+    setTimeout(() => {
+      setAnimateCard([{ y: 0, opacity: 1}]);
+      if(item === 'All') {
+        setFilterWork(works);
+      } else {
+        setFilterWork(works.filter((work) => work.tags.includes(item)))
+      }
+    }, 500);
   }
 
   return (
